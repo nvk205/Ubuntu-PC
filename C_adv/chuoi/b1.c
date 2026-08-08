9e25de975a565a21660b0422671ccfa0acb110c8
nvk205 <nvk.khanh205@gmail.com>
2026-08-08 22:31:04 +0700

    hi

---
+++ C_adv/chuoi/b1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    //khai bao con tro
+    char *ten = "depzai"; //vùng nhớ hằng thì không ghi đè được
+    printf("%s\n", ten);
+    char name[] = "khanh"; 
+    int res = strcmp(ten, name);
+    if (res > 0)
+    {
+        printf("1\n");
+    }
+    else if (res == 0)
+    {
+        printf("0\n");
+    }
+    else
+    {
+        printf("-1\n");
+    }
+
+    printf("%s\n", name);
+    strcpy(&name[4], ten);
+    printf("%s\n", name);
+    return 0;
+}
